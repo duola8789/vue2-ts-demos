957292b5579c0c7e54ff24bab8f4eb58b3b35786
zh <duola8789@126.com>
2019-12-28 18:54:36 +0800

    feat: 添加配置项

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,21 @@
+// ESLint 中文文档：http://eslint.cn/docs/rules/
+// eslint-plugin-vue: https://eslint.vuejs.org/
+// 百度 EFE 团队使用的 eslint 配置：https://github.com/ecomfe/eslint-config/blob/master/index.js
+// JavaScript 编码规范(百度): http://gitlab.baidu.com/fe/spec/blob/master/javascript.md#2-1
+// Vue 组件代码规范(百度): http://gitlab.baidu.com/fe/spec/blob/master/vue.md#2-1
+// iCode校验规则说明：http://bugbye.baidu.com/rules
 module.exports = {
   root: true,
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "@vue/prettier", "@vue/typescript"],
+  extends: [
+    "plugin:vue/recommended",
+    "@vue/prettier",
+    "@vue/typescript",
+    "plugin:prettier/recommended",
+  ],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
   },
   parserOptions: {
     parser: "@typescript-eslint/parser"
