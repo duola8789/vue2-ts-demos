2920a4cad03acc2704aa17941ee9c8c7e569083f
zh <duola8789@126.com>
2019-12-30 17:21:46 +0800

    feat: 添加style-resources-loader，自动导入variables.less中定义的变量

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const StyleLintPlugin = require('stylelint-webpack-plugin');
 
 module.exports = {
@@ -9,5 +10,20 @@ module.exports = {
         lintDirtyModulesOnly: true,
       })
     ]
-  }
+  },
+  chainWebpack: config => {
+    // 自动导入样式文件
+    const types = ['vue-modules', 'vue', 'normal-modules', 'normal'];
+    types.forEach(type => addStyleResource(config.module.rule('less').oneOf(type)))
+  },
 };
+
+function addStyleResource (rule) {
+  rule.use('style-resource')
+    .loader('style-resources-loader')
+    .options({
+      patterns: [
+        path.resolve(__dirname, './src/styles/variables.less'),
+      ],
+    })
+}
