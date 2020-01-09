10022c885a0f00bbfd8f8fd38df04081da7734fe
zh <duola8789@126.com>
2020-01-09 14:29:11 +0800

    feat: 目录优化

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="~@/assets/logo.png" class="logo" />
+    <img alt="Vue logo" src="~@/assets/images/logo.png" class="logo" />
     <h1 class="title">Vue Learning Demos</h1>
   </div>
 </template>
